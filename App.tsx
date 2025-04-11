6c72bc8b12b13fed1ecafce12452dcaa39ffcfec
alnifu <mathew_alex_ulanday@dlsl.edu.ph>
2025-04-11 23:59:40 +0800

    added dark stuff

--- App.tsx
+++ App.tsx
@@ -1,23 +1,36 @@
 // App.tsx
 import React, { useEffect } from 'react';
-import { Alert, Platform } from 'react-native';
+import { Alert, Platform, StatusBar } from 'react-native';
 import * as Notifications from 'expo-notifications';
 import * as Location from 'expo-location';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
-import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider, useTheme } from './context/ThemeContext';
 
+function AppContent() {
+  const { darkMode } = useTheme(); // Access theme context
+
+  return (
+    <>
+      {/* Update the StatusBar based on theme */}
+      <StatusBar
+        barStyle={darkMode ? 'light-content' : 'dark-content'}
+        backgroundColor="transparent"
+        translucent
+      />
+      <BottomTabNavigator />
+    </>
+  );
+}
 
 export default function App() {
   useEffect(() => {
     (async () => {
-      // Request location permissions
       const { status: locationStatus } = await Location.requestForegroundPermissionsAsync();
       if (locationStatus !== 'granted') {
         Alert.alert('Permission Denied', 'Location permission is required to use this app.');
       }
 
-      // Request notification permissions (expo-notifications v0.14.0+)
       const { status: notificationStatus } = await Notifications.requestPermissionsAsync();
       if (notificationStatus !== 'granted') {
         Alert.alert('Permission Denied', 'Notification permission is required to use this app.');
@@ -28,9 +41,8 @@ export default function App() {
   return (
     <ThemeProvider>
       <NavigationContainer>
-        <BottomTabNavigator />
+        <AppContent />
       </NavigationContainer>
     </ThemeProvider>
   );
 }
-
